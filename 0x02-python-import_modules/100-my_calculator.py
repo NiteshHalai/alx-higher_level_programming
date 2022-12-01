504ea9c83493f7b871cfc148f044fae89c84e647
NiteshHalai <nitesh.halai@gmail.com>
2022-12-01 23:40:41 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,14 +13,14 @@ if a[2] not in operators:
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
 if a[2] == '+':
-    print("{} + {} = {}".format(a[1], a[3], add(int(a[1]),int(a[3]))))
+    print("{} + {} = {}".format(a[1], a[3], add(int(a[1]), int(a[3]))))
     exit(0)
 if a[2] == '-':
-    print("{} - {} = {}".format(a[1], a[3], sub(int(a[1]),int(a[3]))))
+    print("{} - {} = {}".format(a[1], a[3], sub(int(a[1]), int(a[3]))))
     exit(0)
 if a[2] == '*':
-    print("{} * {} = {}".format(a[1], a[3], mul(int(a[1]),int(a[3]))))
+    print("{} * {} = {}".format(a[1], a[3], mul(int(a[1]), int(a[3]))))
     exit(0)
 if a[2] == '/':
-    print("{} / {} = {}".format(a[1], a[3], div(int(a[1]),int(a[3]))))
+    print("{} / {} = {}".format(a[1], a[3], div(int(a[1]), int(a[3]))))
     exit(0)
