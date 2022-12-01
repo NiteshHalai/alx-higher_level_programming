6e1759fa94e65f94407c16a3ae083bdebd776353
Nitesh Halai <nitesh.halai@gmail.com>
2022-12-01 11:18:09 -0800

    f

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 import sys
-print(sys.argv)
+arguments = sys.argv
+
+if len(arguments) == 0:
+    print("0 arguments.")
+elif len(arguments) == 1:
+    print("1 argument:")
+    print("1: {}".format(arguments[0]))
+else:
+    print("{} arguments:".format(len(arguments)))
+    for i in range(0, len(arguments)):
+        print("{}: {}".format(i, arguments[i]))
