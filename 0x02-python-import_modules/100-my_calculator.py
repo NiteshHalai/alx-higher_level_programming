8b31a10457af0dba9affd2e24f2fbff66872964a
NiteshHalai <nitesh.halai@gmail.com>
2022-12-01 23:36:16 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,14 +13,14 @@ if arguments[2] not in operators:
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
 if arguments[2] == '+':
-    print("{} + {} = {}".format(arguments[1], arguments[3], add(int(arguments[1]),int(arguments[3])))
+    print("{} + {} = {}".format(arguments[1], arguments[3], add(int(arguments[1]),int(arguments[3]))))
     exit(0)
 if arguments[2] == '-':
-    print("{} - {} = {}".format(arguments[1], arguments[3], sub(int(arguments[1]),int(arguments[3])))
+    print("{} - {} = {}".format(arguments[1], arguments[3], sub(int(arguments[1]),int(arguments[3]))))
     exit(0)
 if arguments[2] == '*':
-    print("{} * {} = {}".format(arguments[1], arguments[3], mul(int(arguments[1]),int(arguments[3])))
+    print("{} * {} = {}".format(arguments[1], arguments[3], mul(int(arguments[1]),int(arguments[3]))))
     exit(0)
 if arguments[2] == '/':
-    print("{} / {} = {}".format(arguments[1], arguments[3], div(int(arguments[1]),int(arguments[3])))
+    print("{} / {} = {}".format(arguments[1], arguments[3], div(int(arguments[1]),int(arguments[3]))))
     exit(0)
