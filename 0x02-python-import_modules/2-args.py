0b1058aa0538aa35c1c04739d5dca4285a7ef5ef
NiteshHalai <nitesh.halai@gmail.com>
2022-12-01 22:24:31 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,5 +11,5 @@ elif len(arguments) == 2:
     print("1: {}".format(arguments[1]))
 else:
     print("{} arguments:".format(len(arguments) - 1))
-    for i in range(0, len(arguments)):
+    for i in range(0, len(arguments) - 1):
         print("{}: {}".format(i + 1, arguments[i + 1]))
