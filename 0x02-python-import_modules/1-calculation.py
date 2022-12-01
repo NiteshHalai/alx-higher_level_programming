5dd6bbe02fada4234df5f768454a3ba9c6a998e0
Nitesh Halai <nitesh.halai@gmail.com>
2022-12-01 10:46:07 -0800

    gh

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1.py import add
-    from calculator_1.py import sub
-    from calculator_1.py import mul
-    from calculator_1.py import div
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
 
 a = 10
 b = 5
