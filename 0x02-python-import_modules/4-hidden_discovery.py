2583def2b979becf0ab288a9fa4c9b22ad3a49c8
NiteshHalai <nitesh.halai@gmail.com>
2022-12-01 22:46:24 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    import hidden_4.pyc
+    import hidden_4
     
-list_of_names = dir(hidden_4.pyc)
+list_of_names = dir(hidden_4)
 
 print (list_of_names)
