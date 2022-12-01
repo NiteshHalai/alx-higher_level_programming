78cbdc75a2d9083c3d2b7b3774594dde10432f1e
NiteshHalai <nitesh.halai@gmail.com>
2022-12-01 23:16:18 +0300

    Update 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    
+arguments = sys.argv
+
+if len(arguments) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    print(1)
