1acdb203f86d4e4cd046a14674d08bdde0abc3b7
NiteshHalai <nitesh.halai@gmail.com>
2022-12-01 22:31:18 +0300

    Update 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+arguments = sys.argv
+total = 0
+for i in range(1, len(arguments) - 1):
+    total = total + int(arguments[i]
+    
+print(total)
