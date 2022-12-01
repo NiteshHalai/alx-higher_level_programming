71a784b7343489bc84094628c0c5f18436485d10
NiteshHalai <nitesh.halai@gmail.com>
2022-12-01 23:34:04 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,9 +2,25 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    
+
 arguments = sys.argv
+operators = ['+', '-', '*', '/']
 
 if len(arguments) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
+if arguments[2] not in operators:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+if arguments[2] == '+':
+    print("{} + {} = {}".format(arguments[1], arguments[3], add(int(arguments[1]),int(arguments[3]))
+    exit(0)
+if arguments[2] == '-':
+    print("{} - {} = {}".format(arguments[1], arguments[3], sub(int(arguments[1]),int(arguments[3]))
+    exit(0)
+if arguments[2] == '*':
+    print("{} * {} = {}".format(arguments[1], arguments[3], mul(int(arguments[1]),int(arguments[3]))
+    exit(0)
+if arguments[2] == '/':
+    print("{} / {} = {}".format(arguments[1], arguments[3], div(int(arguments[1]),int(arguments[3]))
+    exit(0)
