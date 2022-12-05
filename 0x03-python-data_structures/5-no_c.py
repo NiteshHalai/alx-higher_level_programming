d7753d41b902ebf0db53712d042f4e9762ee968a
NiteshHalai <nitesh.halai@gmail.com>
2022-12-05 23:02:10 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    new = []
+    for i in my_string:
+        if i not in ['c', 'C']:
+            new.append(i)
+
+    new = ''.join(new)
+
+    return new
