63ad78afc619fea2895e847f55554999b3326af4
NiteshHalai <nitesh.halai@gmail.com>
2022-12-05 23:34:49 +0300

    Update 11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+
+    if idx < 0:
+        return my_list
+    elif idx >= 0:
+        return my_list
+    elif:
+        new_list = []
+        for i in my_list:
+            if i != idx:
+                new_list.append[my_list[i]]
+
+        return new_list
