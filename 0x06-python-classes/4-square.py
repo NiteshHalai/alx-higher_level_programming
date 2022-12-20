d3d6231d6c5bf1a8face1cca02d372ed66d9b02e
NiteshHalai <nitesh.halai@gmail.com>
2022-12-20 20:54:05 +0300

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a square class
+"""
+
+
+class Square:
+
+    """
+    This is a square class
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        self.area = self.__size * self.__size
+        return self.area
