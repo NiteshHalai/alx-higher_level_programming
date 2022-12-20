8ad556ce752790c83a16af35c7f00d8face5c56b
NiteshHalai <nitesh.halai@gmail.com>
2022-12-20 22:28:31 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,5 +28,5 @@ class Square:
     def my_print(self):
         for i in range(0, self.__size):
             for i in range(0, self.__size):
-                print('#', end = '')
+                print('#', end='')
             print('')
