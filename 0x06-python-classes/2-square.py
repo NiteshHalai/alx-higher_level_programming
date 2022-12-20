b8c5dacea0ab71844c575c1c0a282f765a5de842
NiteshHalai <nitesh.halai@gmail.com>
2022-12-20 20:24:22 +0300

    Create 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a square class
+"""
+
+
+class Square:
+
+    """
+    This is a square class
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
