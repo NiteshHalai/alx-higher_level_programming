2fa939a7348479b654a1279473e3ecd5c04154eb
NiteshHalai <nitesh.halai@gmail.com>
2022-12-20 22:06:14 +0300

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is a square class
+"""
+
+
+class Square:
+
+    """
+    This is a square class
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        self.area = self.__size * self.__size
+        return self.area
