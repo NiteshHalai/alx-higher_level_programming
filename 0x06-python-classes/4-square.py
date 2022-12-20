14314c35cf5ab55c8f7eca72e1395d6d7e2a2c2a
NiteshHalai <nitesh.halai@gmail.com>
2022-12-20 21:06:14 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,12 +12,12 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
-        
+
     def size(self):
         return self.__size
-    
+
     def size(self, value):
-        self.__size = size
+        self.__size = value
 
     def area(self):
         self.area = self.__size * self.__size
