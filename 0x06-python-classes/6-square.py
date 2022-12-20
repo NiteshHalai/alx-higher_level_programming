b532239918ca75bf7857450419ce2f476c7a2de4
NiteshHalai <nitesh.halai@gmail.com>
2022-12-20 22:46:14 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,8 +10,9 @@ class Square:
     This is a square class
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
@@ -21,12 +22,22 @@ class Square:
     def size(self, value):
         self.__size = value
 
+    @property
+    def position(self):
+        return self.__size
+
+    @size.setter
+    def position(self, value):
+        self.__position = value
+
     def area(self):
         self.area = self.__size * self.__size
         return self.area
 
     def my_print(self):
-        for i in range(0, self.__size):
-            for i in range(0, self.__size):
-                print('#', end='')
+        for i in range (0, self.__size):
+            for i in range (0, self.__position[0]):
+                print(' ', end ='')
+            for i in range (0, self.__size):
+                print('#', end ='')
             print('')
