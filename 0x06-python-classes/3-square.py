34abe612e01a45991754b1569038964e006efbc9
NiteshHalai <nitesh.halai@gmail.com>
2022-12-20 20:43:56 +0300

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is a square class
+"""
+
+
+class Square:
+
+    """
+    This is a square class
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+        
+    def area(self):
+        self.area = size * size
