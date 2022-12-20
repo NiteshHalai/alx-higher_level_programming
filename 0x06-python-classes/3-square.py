0667118125a382f28aea17342eed9ba2338bc6bc
NiteshHalai <nitesh.halai@gmail.com>
2022-12-20 20:51:53 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,6 +12,7 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
-        
+
     def area(self):
         self.area = self.__size * self.__size
+        return self.area
