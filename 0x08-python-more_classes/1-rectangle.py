8e3aec538a3417902e4d337a15f431be1ca044e9
NiteshHalai <nitesh.halai@gmail.com>
2023-01-03 20:28:04 +0300

    Create 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is a Rectangle class
+"""
+
+
+class Rectangle:
+
+    """
+    This is a Rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = size
+        self.__height = height
