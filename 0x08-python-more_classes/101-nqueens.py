20935f1a702439ba2415f66d782f6ea2b33e25cf
NiteshHalai <nitesh.halai@gmail.com>
2023-01-04 00:08:46 +0300

    Create 101-nqueens.py

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def nqueens(n):
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (n)
