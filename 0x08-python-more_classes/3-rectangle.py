8b2448f69a09b767d76a2b54256723ef07e0b75b
NiteshHalai <nitesh.halai@gmail.com>
2023-01-03 22:01:00 +0300

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -74,3 +74,9 @@ class Rectangle:
     def perimeter(self):
         self.perimeter = self.__width * 2 + self.__height * 2
         return self.perimeter
+
+    def my_print(self):
+        for i in range(0, self.__height):
+            for i in range(0, self.__width):
+                print('#', end='')
+            print('')
