a11e273394e39a1cf22d4a5fca6dc72f35a09b5c
NiteshHalai <nitesh.halai@gmail.com>
2023-01-03 21:32:30 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,5 +11,58 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        try:
+            self.__width = width
+            if width < 0:
+                raise ValueError
+            if type(width) is not int:
+                raise TypeError
+        except TypeError:
+            raise TypeError("width must be an integer")
+        except ValueError:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        try:
+            self.__width = value
+            if value < 0:
+                raise ValueError
+            if type(value) is not int:
+                raise TypeError
+        except TypeError:
+            raise TypeError("width must be an integer")
+        except ValueError:
+            raise ValueError("width must be >= 0")
+            
+         try:
+            self.__height = height
+            if height < 0:
+                raise ValueError
+            if type(height) is not int:
+                raise TypeError
+        except TypeError:
+            raise TypeError("height must be an integer")
+        except ValueError:
+            raise ValueError("height must be >= 0")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        try:
+            self.__height = value
+            if value < 0:
+                raise ValueError
+            if type(value) is not int:
+                raise TypeError
+        except TypeError:
+            raise TypeError("height must be an integer")
+        except ValueError:
+            raise ValueError("height must be >= 0")
