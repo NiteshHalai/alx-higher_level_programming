099a4809bf36afbc9c715bfed70458e559d00d62
NiteshHalai <nitesh.halai@gmail.com>
2023-01-05 22:45:03 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def nqueens(n):
-    """My addition function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a + b
-    """
-    argv = open('/proc/self/cmdline').read().split('\0')
-    print(argv)
-    print(n)
-    return (n)
+import sys 
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
