c51354be596c5e19b5ad1bb8e27160c67e0a0d3f
NiteshHalai <nitesh.halai@gmail.com>
2023-01-09 22:43:24 +0300

    Update 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is a class
+"""
+
+
+class list:
+    """
+    This is a class
+    """
+    pass
+    
+class MyList(list):
+    """
+    This is a function
+    """
+    
+    def print_sorted(self):
+        class list sort()
