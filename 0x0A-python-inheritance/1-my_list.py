132239cdf71c491f613fec5165c49933370e6e04
Nitesh Halai <nitesh.halai@gmail.com>
2023-01-09 12:12:30 -0800

    drf

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """
 This is a class
@@ -18,28 +17,3 @@ class MyList(list):
     
     def print_sorted(self):
         class list sort()
-=======
-#!/usr/bin/python3
-"""
-This is a class
-"""
-
-
-class list:
-    """
-    This is a class
-    """
-    pass
-    
-class MyList(list):
-    """
-    This is a function
-    """
-    
-    def print_sorted(self):
-        
-    """
-    This is a function
-    """
-        return self.sort()
->>>>>>> b772975478a7f333cbf4b350e3fb6914870b6db0
