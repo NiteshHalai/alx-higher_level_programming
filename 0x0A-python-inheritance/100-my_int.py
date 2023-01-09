b5d4fd65ab5980be2bf50b89bd39ea9c8ad595e9
NiteshHalai <nitesh.halai@gmail.com>
2023-01-10 01:06:48 +0300

    Update 100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is a class
+"""
+
+
+class MyInt(int):
+    """
+    This is a class
+    """
+
+    def equal(self):
+        """ This is my function """
+        if self == 3:
+            return False
+        else:
+            return True
