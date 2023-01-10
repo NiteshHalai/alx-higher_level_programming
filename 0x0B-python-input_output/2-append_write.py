842a7bd7473cfb419c0b81f78fbfdd37c6aef09e
NiteshHalai <nitesh.halai@gmail.com>
2023-01-10 21:44:17 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 
 def append_write(filename="", text=""):
     """Function description here"""
-    with open(filename, access_mode="a", encoding="utf-8") as f:
-        f.write(text)
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
