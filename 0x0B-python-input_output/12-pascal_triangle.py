5067d80d45ac883e3b13e174d370c3321afef8b2
NiteshHalai <nitesh.halai@gmail.com>
2023-01-11 00:21:58 +0300

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,4 +13,5 @@ def pascal_triangle(n):
         for i in range (0, n):
             rows.append(11**i)
         print(rows)
+        print("[{}]".format(",".join([str(x) for x in rows])))  
         return rows
