dc0155fd624ce39106a903bdf8334d24c4f77c35
NiteshHalai <nitesh.halai@gmail.com>
2023-01-10 20:41:03 +0300

    Update 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Function description here"""
+
+
+def read_file(filename=""):
+    """Function description here"""
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
+        return read_data
