ffd6a93a90f3976a4226c682797bcb237a2c471d
NiteshHalai <nitesh.halai@gmail.com>
2023-01-10 23:58:24 +0300

    Update 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is a Student class
+"""
+
+
+class Student:
+
+    """
+    This is a Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dict = self.__dict__
+        if attrs is None:
+            return dict
+        else:
+            return {key: dict[key] for key in attrs}
+            
+    def reload_from_json(self, json)"
+        pass
