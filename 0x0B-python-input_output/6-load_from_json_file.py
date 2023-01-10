5b2c8ed9baa69dc825151c5a3093a9f93fc3f5b8
NiteshHalai <nitesh.halai@gmail.com>
2023-01-10 22:19:22 +0300

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,10 +8,8 @@ import json
 
 def load_from_json_file(filename):
     """Function description here"""
-    
+
     with open(filename, encoding="utf-8") as f:
         json_str = f.read()
 
     return json.loads(json_str)
-
-    
