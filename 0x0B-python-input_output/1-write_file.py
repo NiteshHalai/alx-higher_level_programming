36e13112c5c88c2c97a0cddb0563afade504b6f7
NiteshHalai <nitesh.halai@gmail.com>
2023-01-10 21:38:08 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 
 def write_file(filename="", text=""):
     """Function description here"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, encoding="utf-8", access_mode="w") as f:
         f.write(text)
