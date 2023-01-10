15830fb0529e60c85c29dbeba40790bd6c1f02ad
NiteshHalai <nitesh.halai@gmail.com>
2023-01-10 20:50:34 +0300

    Update 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Function description here"""
+
+
+def write_file(filename="", text=""):
+    """Function description here"""
+    with open(filename, encoding="utf-8") as f:
+        f.write(text)
