523ed0ad8fa752fb4d2c8b88eeb1ebd888c01efe
NiteshHalai <nitesh.halai@gmail.com>
2023-01-16 23:21:58 +0300

    Create square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is a class
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    This is a class
+    """
+    
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(height, x, y, id)
+        
+    def __str__(self):
+        """Function description here"""
+        return "[Square] (" + str(self.id) + ") " + str(self.__x) + "/" + str(self.__y) + " - " + str(self.__size)
