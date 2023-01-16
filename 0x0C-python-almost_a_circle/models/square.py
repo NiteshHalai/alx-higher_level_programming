e9d002d7e65faaf4bcb2ad02aa5a73a3a76c2d22
NiteshHalai <nitesh.halai@gmail.com>
2023-01-16 23:43:37 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,10 +10,23 @@ class Square(Rectangle):
     """
     This is a class
     """
-    
+
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        
+
     def __str__(self):
         """Function description here"""
-        return "[Square] (" + str(self.id) + ") " + str(self.x) + "/" + str(self.y) + " - " + str(self.width)
+        id1 = str(self.id)
+        x = str(self.x)
+        y = str(self.y)
+        w = str(self.width)
+        return "[Square] (" + id1 + ") " + x + "/" + y + " - " + w
+
+        @property
+        def size(self):
+            return self.width
+
+        @height.setter
+        def width(self, value):
+            self.width = value
+            self.height = value
